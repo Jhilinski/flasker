a6cfdd4c3f6abc0b628e061b74187f82f0f6fc2f
Jhilinski <jhilinski@yahoo.com>
2025-06-06 16:52:03 -0400

    First commit

--- hello.py
+++ hello.py
@@ -33,8 +33,17 @@ def index():
 def user(name):
     return render_template("user.html",user_name=name)
 
+# Create Custom Error Pages
 
+# Invalid URL
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
 
+# Internal Server Error
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("500.html"), 500
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000, debug=True)
